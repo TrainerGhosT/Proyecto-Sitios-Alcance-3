6fd6774c0e9fcaa0abdb0cdfd2c49e86d195c7d5
TrainerGhosT <dylanmont15@gmail.com>
2025-04-21 15:45:37 -0600

    Update Oferente Interface

--- frontend/src/types/IOferente.ts
+++ frontend/src/types/IOferente.ts
@@ -1,5 +1,17 @@
-export interface Oferente {
+export interface OferenteBase {
   
   nombre: string;
   identificacion: string;
+}
+
+export interface Oferente extends OferenteBase {
+  IdOferente: number;
+  Direccion: string;
+  TipoIdentificacion: string;
+  FechaNacimiento: Date;
+  EstadoCivil: string;
+  Nacionalidad: string;
+  IdDistrito: number;
+  Curriculum: string;
+
 }
